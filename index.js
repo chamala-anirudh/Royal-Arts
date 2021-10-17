db35e34a5317d1c15dab13ba2f2491a7c7d067c1
Anirudh Chamala <52962354+chamala-anirudh@users.noreply.github.com>
2021-10-17 10:41:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// const navbar = document.querySelector("nav");
+// const nav_items = document.querySelector(".navigation_item");
+// const close = document.querySelector(".navigation_checkbox");
+// nav_items.addEventListener("click", function (e) {});
